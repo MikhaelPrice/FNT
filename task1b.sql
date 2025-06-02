4a8ab5716c449c844704fe64ee3bd1d4a0f498ec
MikhaelPrice <90347499+MikhaelPrice@users.noreply.github.com>
2025-06-02 14:15:33 +0400

    Updated with zero sales values in target period (Jan 1, 2020 - March 1, 2020)

--- task1b.sql
+++ task1b.sql
@@ -1,14 +1,18 @@
 SELECT
     vmk.id AS make_id,
     vmk.name AS make_name,
-    COUNT(a.id) AS num_vehicles_sold
+    COUNT(ml.id) AS num_vehicles_sold
 FROM
     `asset-schema`.vehicle_make vmk
-LEFT JOIN `asset-schema`.vehicle_model vml ON vmk.id = vml.vehicle_make_id
-LEFT JOIN `asset-schema`.asset a ON vml.id = a.model_id
-LEFT JOIN `loan-schema`.m_loan ml ON a.m_loan_id = ml.id
+LEFT JOIN `asset-schema`.vehicle_model vml
+    ON vmk.id = vml.vehicle_make_id
+LEFT JOIN `asset-schema`.asset a
+    ON vml.id = a.model_id
+LEFT JOIN `loan-schema`.m_loan ml
+    ON a.m_loan_id = ml.id
     AND ml.disbursedon_date >= '2020-01-01'
-    AND ml.disbursedon_date <  '2020-03-01'
+    AND ml.disbursedon_date < '2020-03-01'
 GROUP BY
     vmk.id, vmk.name
-ORDER BY num_vehicles_sold DESC;
+ORDER BY
+    num_vehicles_sold DESC;
